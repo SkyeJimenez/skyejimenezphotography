313bb114023cd76fcc421b3ffb673563612d2eed
SkyeJimenez <skyejimenez22@gmail.com>
2026-05-12 17:45:21 -0700

    Add event listener to button for feedback message

--- js/script.js
+++ js/script.js
@@ -16,7 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     if (greetingEl) {
         greetingEl.textContent = greeting;
     }
+// 1. Select the elements
+var elBtn = document.getElementById('event-btn');
+var elNote = document.getElementById('feedback');
 
+// 2. Define the function (what happens when the event fires)
+function showMessage() {
+  elNote.textContent = 'The event was successfully triggered!';
+}
+
+// 3. Add the event listener
+// Note: We don't use 'on' (like onclick); we use the event name 'click'
+elBtn.addEventListener('click', showMessage, false);
     //EVENT (CLICK EVENT)
     const button = document.getElementById("myButton");
 
